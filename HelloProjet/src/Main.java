71aa788fd21842bd18bda600a4f6e1437a7c1983
root <631953504@qq.com>
2019-08-06 19:13:10 +0800

    本次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("提交新代码到服务器1");
+        System.out.println("提交新代码到服务器2");
     }
 }
