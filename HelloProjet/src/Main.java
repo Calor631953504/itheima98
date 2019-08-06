ee20f252ed1dc23f5e89340608f04aef8da6b279
root <631953504@qq.com>
2019-08-06 20:16:35 +0800

    222

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("提交新代码到服务器4");
         System.out.println("提交新代码到服务器5");
+        System.out.println("提交新代码到服务器6");
     }
 }
